83cbf01a38459f83717320519f36652884157e89
Achintya Rao <achintya.rao@cern.ch>
2016-03-21 13:40:49 +0100

    Update CMS Research intro text

--- invenio_opendata/base/fixtures/websearch.py
+++ invenio_opendata/base/fixtures/websearch.py
@@ -648,7 +648,7 @@ class PortalboxData(DataSet):
         title = u'description'
 
     class Portalbox_17:
-        body = u'CMS is releasing data in the same format as used in data analysis by CMS physicists. A CMS-specific analysis framework is needed, and it is provided as a Virtual Machine image with the CMS analysis environment. The data can be accessed directly through the VM image. Basic information of the data contents is provided in <a href="http://opendata.cern.ch/about/CMS">About CMS</a> and in <a href="http://opendata.cern.ch/about/CMS-Physics-Objects">About CMS Physics Objects</a>. The original data are in primary datasets, i.e. no selection nor identification criteria have been applied (apart from the trigger decision), and these have to be applied in the subsequent analysis step. For the first release, no simulated Monte Carlo datasets are provided'
+        body = u'CMS Open Data are available in the same format as used in analysis by CMS physicists. A CMS-specific analysis framework is needed, and it is provided as a Virtual Machine image with the CMS analysis environment. The data can be accessed directly through the VM image. Basic information of the data contents is provided in <a href="http://opendata.cern.ch/about/CMS">About CMS</a> and in <a href="http://opendata.cern.ch/about/CMS-Physics-Objects">About CMS Physics Objects</a>. The original data are in primary datasets, i.e. no selection nor identification criteria have been applied (apart from the trigger decision), and these have to be applied in the subsequent analysis step. The 2011 data release includes simulated Monte Carlo datasets, but no simulated datasets are provided for the 2010 release.'
         id = 17
         title = u'research_description'
 
