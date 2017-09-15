03206fa851c9a7864655640abd128b724a9382fb
Tibor Simko <tibor.simko@cern.ch>
2017-09-15 10:06:26 +0200

    travis: test Docker image building
    * Plugs Docker image building into Travis CI.
    
    * Simplifies Travis CI testing matrix to use only Python 2.7 (the one necessary
      for XRootD).
    
    * Removes {lowest,release,devel} from Travis CI testing matrix that we would
      need to add to execute inside the containerised environment.
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -25,6 +25,4 @@
 pydocstyle cernopendata && \
 isort -rc -c -df **/*.py && \
 check-manifest --ignore ".travis-*" && \
-# sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test # && \
-# sphinx-build -qnNW -b doctest docs docs/_build/doctest
+python setup.py test
