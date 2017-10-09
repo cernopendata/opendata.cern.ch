f0e32025ed26bafa2a3f88b963a3ddcffbad56f7
Anna Trzcinska <anna.trzcinska@cern.ch>
2017-10-09 14:41:30 +0200

    facets: fix facets and urls after clicking labels
    Signed-off-by: Anna Trzcinska <anna.trzcinska@cern.ch>

--- cernopendata/config.py
+++ cernopendata/config.py
@@ -315,6 +315,7 @@ SEARCH_UI_SEARCH_API = "/api/records/"
 # SEARCH_UI_BASE_TEMPLATE = 'cernopendata/page.html'
 #: Default template for search UI.
 SEARCH_UI_SEARCH_TEMPLATE = 'cernopendata/search.html'
+SEARCH_UI_JSTEMPLATE_FACETS = 'templates/cernopendata_search_ui/facets.html'
 #: Default Elasticsearch document type.
 SEARCH_DOC_TYPE_DEFAULT = None
 #: Do not map any keywords.
