335c72dd9296db0c4c0cf88851d7d5e7204bc787
Pablo Saiz <pablo.saiz@cern.ch>
2024-03-04 17:01:33 +0100

    portal: Take the code of the portal to a different repo

--- cernopendata/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""CERN Open Data Modules."""
--- cernopendata/wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""cernopendata Invenio WSGI application."""
-
-from .factory import create_app
-
-application = create_app()
